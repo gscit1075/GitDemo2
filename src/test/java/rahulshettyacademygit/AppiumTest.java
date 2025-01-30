d2ad06aa00a63a5d6645e0499fe206f263a803c6
mahesh <mahesh.gscit1075@gmail.com>
2025-01-30 17:37:15 +0530

    PST time zone guy commit

---
+++ src/test/java/rahulshettyacademygit/AppiumTest.java
@@ -0,0 +1,25 @@
+package rahulshettyacademygit;
+
+import org.testng.annotations.Test;
+
+public class AppiumTest {
+	
+	@Test
+	
+	public void nativeAPPAndroid()
+	{
+		System.out.println("nativeAPPAndroid");
+		
+	}
+	
+	@Test
+	
+	public void iOSApp()
+	{
+		System.out.println("diOSApp");
+	}
+
+	
+	
+
+}
---
+++ src/test/java/rahulshettyacademygit/SeleniumTest.java
@@ -0,0 +1,23 @@
+package rahulshettyacademygit;
+
+import org.testng.annotations.Test;
+
+public class SeleniumTest {
+	
+	@Test
+	public void BrowserAutomation()
+	{
+	System.out.println("BrowserAutomation");
+	
+	
+	}
+	
+	@Test
+	public void elementUI()
+	{
+	System.out.println("elementUI");
+	
+	
+	}
+
+}
